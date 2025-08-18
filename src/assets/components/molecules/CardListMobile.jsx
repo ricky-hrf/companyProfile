e9973e483fdb722f122a09d71c99e7626673fce3
Ricky Harefa <114171685+ricky-hrf@users.noreply.github.com>
2025-08-18 15:13:47 +0700

    clean code

---
+++ src/assets/components/molecules/CardListMobile.jsx
@@ -0,0 +1,21 @@
+const CardListMobile = (judul, lists, open) => {
+  return (
+    <div>
+      {open && (
+        <div className="">
+      <div className="origin-top absolute right-0 left-0 md:hidden bg-orange-50 p-2 shadow-xl transform transition-all duration-300 ease-in-out">
+        <div className="">{judul}</div>
+      </div>
+      <div>
+        {lists.map((item, index) => (
+          <div className="" key={index}>{item}</div>
+        ))}
+      </div>
+        </div>
+
+      )}
+    </div>
+    
+  );
+}
+export default CardListMobile;
